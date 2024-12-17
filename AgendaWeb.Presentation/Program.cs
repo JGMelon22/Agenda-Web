c9169bdafe2cbf9811870bcd920295619b26fdc6
Ryan Mamede <85031572+Ryan-Mamede@users.noreply.github.com>
2024-12-17 16:47:44 -0300

    Primeiro commit

---
+++ AgendaWeb.Presentation/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurando o projeto para MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Definindo a página inicial do projeto
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+    );
+
+app.Run();
